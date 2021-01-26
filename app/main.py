b3241de406044c1bd12c1dadf1425b3de0be0ad5
Shinichi Kawabata <hammamania@gmail.com>
2021-01-26 23:35:03 +0900

    Initial commit.

---
+++ app/main.py
@@ -0,0 +1,92 @@
+import os
+
+from flask import flash, Flask, render_template, request, redirect, url_for, send_file, after_this_request
+
+app = Flask(__name__)
+app.secret_key = b'mypass'
+
+# ----------------------------------------------------------------------
+# トップページ
+# ----------------------------------------------------------------------
+@app.route('/')
+def index():
+    """
+    インデックスページを表示する。
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    render_template ： function
+        表示するWebページ
+
+    Notes
+    -----
+        This function is decorated app.route.
+        Routing to '/' page.
+    """
+    return render_template(
+        'index.html',
+        title="RunOF7-GUI"
+    )
+
+# ----------------------------------------------------------------------
+# トップページ
+# ----------------------------------------------------------------------
+@app.route('/run', methods=['POST'])
+def runOpenFOAM():
+    """
+    インデックスページを表示する。
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    render_template ： function
+        表示するWebページ
+
+    Notes
+    -----
+        This function is decorated app.route.
+        Routing to '/' page.
+    """
+    import subprocess as sp
+    import shutil
+    import datetime
+
+    # 一時ファイルの作成
+    now = datetime.datetime.now()
+    timeStomp = now.strftime('%Y%m%d_%H%M%S')
+
+    f = request.files['mesh']
+    folder_path = os.path.join("/mnt", "windows", "case"+timeStomp)
+    os.mkdir(folder_path)
+    zip_path = os.path.join(folder_path, f.filename)
+    f.save(zip_path)
+
+    pwd = os.getcwd()
+
+    try:
+        os.chdir(folder_path)
+        sp.check_call(["unzip", f.filename])
+        sp.check_call(["rm", f.filename])
+        sp.check_call(["chmod", "755", "Allrun"])
+        sp.check_call(["bash", "-c",  """. /opt/openfoam7/etc/bashrc;./Allrun"""])
+        os.chdir(pwd)
+        flash("Success run.", 'info')
+    except:
+        os.chdir(pwd)
+        flash("Don't success run.", 'error')
+
+    return redirect(url_for('index'))
+
+
+# ----------------------------------------------------------------------
+# メインルーチン
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+    installDir = os.getcwd()
+    app.run(host='0.0.0.0', port=8080,
+        debug=True, use_reloader=False, use_debugger=True)
